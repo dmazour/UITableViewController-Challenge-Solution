03d258f81c2521b5d2fdce4765d40a232cac415b
Daniel Mazour <dmazour@Daniels-MBP.home>
2015-06-08 13:20:30 -0400

    second commit

---
+++ UITableViewController Challenge Solution/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  
+//
+//  Created by Daniel Mazour on 6/8/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
